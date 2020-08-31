949f08138b46dea7a26fa05978c0d7665a77e75b
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-08-31 02:09:08 +0200

    Replacing and using BAT0/capacity insead

--- batteryfull.sh
+++ batteryfull.sh
@@ -2,12 +2,12 @@
 while true
 do
     export DISPLAY=:0.0
-    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    battery_percent=$(cat /sys/class/power_supply/BAT0/capacity)
     if on_ac_power; then
-        if [ "$battery_percent" -gt 95 ]; then
+        if [ "$battery_percent" -gt 96 ]; then
             notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
             paplay /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
         fi
     fi
-    sleep 300 # (5 minutes)
+    sleep 240 # 4 minutes
 done
