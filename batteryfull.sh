4cecfe2dc2e0331f3f32b4f6c2895b3823fc430c
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-08-31 02:44:09 +0200

    Adding some features! I just forgot what I added

--- batteryfull.sh
+++ batteryfull.sh
@@ -5,7 +5,7 @@
 # making a notif function 
 
 notify_me () {
-	notify-send -u critical -t 0 -i "$PWD/batteryfull.png" "$1" "$2"
+	notify-send -u critical -t 0 -i "$PWD/batteryfull.png" "$1" "Level : $2%"
 	paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
 }
 
@@ -14,8 +14,8 @@ do
     export DISPLAY=:0.0
     battery_percent=$(cat /sys/class/power_supply/BAT0/capacity)
     if on_ac_power; then
-        if [ "$battery_percent" ">" "94" ]; then
-		notify_me "Battery is almost fully charged." "You can unplug the charger now"
+        if [ $battery_percent -gt 94 ]; then
+		notify_me "You can unplug the charger now! the battery is almost full" $battery_percent
         fi
     fi
     sleep 240 # 4 minutes
