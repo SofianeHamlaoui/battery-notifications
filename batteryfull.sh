dc1f74f7fa54768a715871ed79c3572707be2745
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-08-31 02:34:37 +0200

    Making the script better

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+
+
+
+# making a notif function 
+
+notify_me () {
+	notify-send -u critical -t 0 -i "$PWD/batteryfull.png" "$1" "$2"
+	paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
+}
+
 while true
 do
     export DISPLAY=:0.0
     battery_percent=$(cat /sys/class/power_supply/BAT0/capacity)
     if on_ac_power; then
-        if [ "$battery_percent" -gt 96 ]; then
-            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
-	    paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
+        if [ "$battery_percent" ">" "94" ]; then
+		notify_me "Battery is almost fully charged." "You can unplug the charger now"
         fi
     fi
     sleep 240 # 4 minutes
